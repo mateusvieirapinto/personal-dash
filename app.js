2864ce96b953e426bd44e572faceedd272ebe18f
mateusvieirapinto <125665271+mateusvieirapinto@users.noreply.github.com>
2026-03-30 22:41:13 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// Adiciona a classe "active" ao botão menu-h quando clicado com o mouse:
+document.querySelector(".menu-h").addEventListener("click", () => {
+  document.querySelector(".menu-nav").classList.toggle("active");
+});
+// Fazendo com que a classe menu-h ganhe uma classe adicional "active" trazendo novos atributos visuais quando o botão for clicado.
